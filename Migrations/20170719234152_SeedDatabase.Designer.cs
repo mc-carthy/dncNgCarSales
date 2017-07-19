a4f483bb834e6efba8e813e52fb6d44d9bc6ee5a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-20 00:52:42 +0100

    Seed database

---
+++ Migrations/20170719234152_SeedDatabase.Designer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using dncNgCarSales.Persistence;
+
+namespace Skeleton.Migrations
+{
+    [DbContext(typeof(SkeletonDbContext))]
+    [Migration("20170719234152_SeedDatabase")]
+    partial class SeedDatabase
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("dncNgCarSales.Models.Make", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Makes");
+                });
+
+            modelBuilder.Entity("dncNgCarSales.Models.Model", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("MakeId");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("MakeId");
+
+                    b.ToTable("Models");
+                });
+
+            modelBuilder.Entity("dncNgCarSales.Models.Model", b =>
+                {
+                    b.HasOne("dncNgCarSales.Models.Make", "Make")
+                        .WithMany("Models")
+                        .HasForeignKey("MakeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ Migrations/20170719234152_SeedDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Skeleton.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('Audi')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('BMW')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) Values ('Mercedes')");
+            
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('A3', (SELECT ID FROM Makes WHERE Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('A5', (SELECT ID FROM Makes WHERE Name = 'Audi'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('A6', (SELECT ID FROM Makes WHERE Name = 'Audi'))");
+            
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('1-Series', (SELECT ID FROM Makes WHERE Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('5-Series', (SELECT ID FROM Makes WHERE Name = 'BMW'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('3-Series', (SELECT ID FROM Makes WHERE Name = 'BMW'))");
+            
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('A-Class', (SELECT ID FROM Makes WHERE Name = 'Mercedes'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('C-Class', (SELECT ID FROM Makes WHERE Name = 'Mercedes'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeId) Values ('E-Class', (SELECT ID FROM Makes WHERE Name = 'Mercedes'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name IN ('Audi', 'BMW', 'Mercedes')");
+        }
+    }
+}
