29dcba4a6f904c9b164aedae074c3df8a2d14ad9
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-19 23:58:39 +0100

    Import connection strings through Configuration

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace Skeleton
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SkeletonDbContext>(options => options.UseSqlServer("..."));
+            services.AddDbContext<SkeletonDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             // Add framework services.
             services.AddMvc();
         }
