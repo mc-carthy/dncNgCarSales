264ea1befc86d9d509952d208d6b96ad9bd023da
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-20 18:23:10 +0100

    Implement Delete action for Vehicle (and apply more error checking to Update)

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -48,6 +48,12 @@ namespace dncNgCarSales.Controllers
             }
 
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -56,6 +62,22 @@ namespace dncNgCarSales.Controllers
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
         }
     }
 }
