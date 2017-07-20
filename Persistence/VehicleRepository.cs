372113919b180e29c4b93c8e7da043fdf4c3e963
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-20 21:30:09 +0100

    Create a basic repository for GetVehicle

---
+++ Persistence/VehicleRepository.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using dncNgCarSales.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dncNgCarSales.Persistence
+{
+    public class VehicleRepository
+    {
+        private readonly SkeletonDbContext context;
+        public VehicleRepository(SkeletonDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Vehicle> GetVehicle(int id)
+        {
+            return await context.Vehicles
+                .Include(v => v.Features)
+                    .ThenInclude(vf => vf.Feature)
+                .Include(v => v.Model)
+                    .ThenInclude(m => m.Make)
+                .SingleOrDefaultAsync(v => v.Id == id);
+        }
+    }
+}
