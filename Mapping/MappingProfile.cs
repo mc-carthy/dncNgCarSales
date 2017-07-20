825a9f6721af95fe45f7dcba0b31c70fedb0ecc0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-20 18:12:58 +0100

    Refactor AddFeatures section of mapping to make it more test friendly

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -36,17 +36,24 @@ namespace dncNgCarSales.Mapping
                 .ForMember(v => v.Features, opt => opt.Ignore())
                 .AfterMap((vr, v) => {
                     // Remove unselected features
-                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
+                    var removedFeatures = v.Features
+                        .Where(f => !vr.Features
+                        .Contains(f.FeatureId));
+
                     foreach (var f in removedFeatures)
                     {
                         v.Features.Remove(f);
                     }
 
                     // Add new features
-                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id));
-                    foreach (var id in addedFeatures)
+                    var addedFeatures = vr.Features
+                        .Where(id => !v.Features
+                        .Any(f => f.FeatureId == id))
+                        .Select(id => new VehicleFeature { FeatureId = id});
+                        
+                    foreach (var f in addedFeatures)
                     {
-                        v.Features.Add(new VehicleFeature { FeatureId = id });                        
+                        v.Features.Add(f);                        
                     }
                 });
         }
