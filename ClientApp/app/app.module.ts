87be82d4cbc8dca407752b43150f6e245fe2b404
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-20 12:21:38 +0100

    Create basic Make service

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+
 import { AppComponent } from './components/app/app.component'
+
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+
+import { MakeService } from './services/make.service';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -28,6 +32,9 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
+    ],
+    providers: [
+        MakeService
     ]
 })
 export class AppModule {
