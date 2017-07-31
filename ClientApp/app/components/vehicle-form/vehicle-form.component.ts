a0a5f3883a58c6e9d28787d40b2f08ea9f0bb193
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-31 21:44:56 +0100

    Populate form

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -59,6 +59,7 @@ export class VehicleFormComponent implements OnInit {
             this.features = data[1];
             if (this.vehicle.id) {
                 this.setVehicle(data[2]);
+                this.populateModels();
             }
         }, err => {
             if (err.status == 404) {
@@ -78,9 +79,13 @@ export class VehicleFormComponent implements OnInit {
     }
 
     onMakeChange() {
+        this.populateModels();
+        delete this.vehicle.modelId;
+    }
+
+    private populateModels() {
         var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
         this.models = selectedMake ? selectedMake.models : [];
-        delete this.vehicle.modelId;
     }
 
     onFeatureToggle(featureId, $event)
