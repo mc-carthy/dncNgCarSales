22d47e468986a7e99e2c82e29392fbec13f41053
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-31 21:20:35 +0100

    Redirect to home if invalid id is passed to vehicles/:id

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -34,6 +34,11 @@ export class VehicleFormComponent implements OnInit {
         this.vehicleService.getVehicle(this.vehicle.id)
             .subscribe(v => {
                 this.vehicle = v;
+            }, err => {
+                if (err.status == 404)
+                    {
+                        this.router.navigate(['home']);
+                    }
             });
 
         this.vehicleService.getMakes().subscribe(makes => {
